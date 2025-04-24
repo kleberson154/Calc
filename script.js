f61ce7595618c0de8a2505fb2db4fb768e32f6ae
kleberson154 <kleberson55@hotmail.com>
2025-04-24 13:15:37 -0300

    modify

--- script.js
+++ script.js
@@ -1,6 +1,12 @@
 const toggle = document.querySelector('.toggle')
 const buttonToggle = document.querySelector('.buttonToggle')
-let displayDown = document.querySelector('.')
+let displayDown = document.querySelector('.displayDown')
+const btnOne = document.querySelector('#btn-1')
+const btnZero = document.querySelector('#btn-0')
+const btnDivide = document.querySelector('#btn-divide')
+const btnMultiply = document.querySelector('#btn-multiply')
+const btnMinus = document.querySelector('#btn-minus')
+const btnPlus = document.querySelector('#btn-plus')
 
 buttonToggle.style.left == '0px'
 toggle.addEventListener('click', () => {
@@ -10,5 +16,13 @@ toggle.addEventListener('click', () => {
     buttonToggle.style.left = '38px'
   } else {
     buttonToggle.style.left = '0'
+  }
+})
+
+btnOne.addEventListener('click', e => {
+  if (e.key === '1') {
+    newSpan = document.createElement('span')
+    newSpan.innerText = '1'
+    displayDown.appendChild(newSpan)
   }
 })
